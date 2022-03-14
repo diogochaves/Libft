1640b0954b5b9ff5403b55b822dfa466445f0ac5
Diogo Chaves <diogo@enfoquevisual.com.br>
2022-03-14 21:50:23 +0100

    Fix ft_split

--- src/ft_split.c
+++ src/ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dchaves- <dchaves-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dchaves- <dchaves-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 15:58:41 by dchaves-          #+#    #+#             */
-/*   Updated: 2021/09/14 15:58:42 by dchaves-         ###   ########.fr       */
+/*   Updated: 2022/03/14 21:48:56 by dchaves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,22 @@ static void	create_tokens(char **group, const char *s, char c)
 {
 	size_t	i;
 	size_t	j;
+	size_t	len;
 	int		pos;
 
 	i = 0;
 	j = 0;
+	len = ft_strlen(s);
 	pos = -1;
-	while (i <= ft_strlen(s))
+	while (i <= len)
 	{
 		if (s[i] != c && pos == -1)
 			pos = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && pos != -1)
+		else if ((s[i] == c || i == len) && pos != -1)
 		{
-			group[j] = ft_substr(s, pos, i - pos);
+			group[j] = malloc(sizeof(char) * (i - pos + 1));
+			if (group[j])
+				ft_strlcpy(group[j], s + pos, i - pos + 1);
 			j++;
 			pos = -1;
 		}
